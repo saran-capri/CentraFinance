72697e0c5ebe613adfd81993cab7168a2041e980
samathaguddepogu24@gmail.com <samathaguddepogu24@gmail.com>
2024-08-06 02:21:42 -0400

    Updated Progress.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -140,7 +140,7 @@ const AuthForm = ({ type }: { type: string}) => {
                   <CustomInput control={form.control} name='address1' label="Address" placeholder='Enter your specific address' />
                   <CustomInput control={form.control} name='city' label="City" placeholder='Enter your city' />
                 <div className="flex gap-4">
-                  <CustomInput control={form.control} name='state' label="State" placeholder='Example: NY' />
+                  <CustomInput control={form.control} name='state' label="State" placeholder='Example: ON' />
                   <CustomInput control={form.control} name='postalCode' label="Postal Code" placeholder='Example: 11101' />
                 </div>
                 <div className="flex gap-4">
--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -2,13 +2,18 @@
 
 import * as React from "react"
 import * as ProgressPrimitive from "@radix-ui/react-progress"
-
 import { cn } from "@/lib/utils"
+
+// Define a new interface to include indicatorClassName
+interface CustomProgressProps extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorClassName?: string;
+  value: number;
+}
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->(({indicatorClassName, className, value, ...props }, ref) => (
+  CustomProgressProps
+>(({ indicatorClassName, className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
@@ -22,7 +27,8 @@ const Progress = React.forwardRef<
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-))
-Progress.displayName = ProgressPrimitive.Root.displayName
+));
+
+Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress }
